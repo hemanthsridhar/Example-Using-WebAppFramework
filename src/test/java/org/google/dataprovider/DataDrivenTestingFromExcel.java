ec1e0ac3bf18e5663c706ed508663664bbe76f67
Hemanth Sridhar <hemanthsridhar@Hemanths-MacBook-Pro.local>
2016-07-31 19:27:54 +0530

    added examples on how to read from excel and updated to the latest webappframework 1.3.2

--- src/test/java/org/google/dataprovider/DataDrivenTestingFromExcel.java
+++ src/test/java/org/google/dataprovider/DataDrivenTestingFromExcel.java
@@ -2,6 +2,8 @@ package org.google.dataprovider;
 
 import java.io.File;
 import java.lang.reflect.Method;
+
+import org.framework.utils.ApplicationSetUpPropertyFile;
 import org.framework.utils.ExcelLibrary;
 import org.testng.annotations.DataProvider;
 
@@ -10,9 +12,19 @@ public class DataDrivenTestingFromExcel {
 	
 	@DataProvider(name="excelSheetDataRead",parallel = true)
 	public static Object[][] excelSheetDataRead(Method methodName) throws Exception{
-		File file = new File("resources/ExcelSheetData/"+methodName.getName()+".xlsx");
+		File file = new File("resources/"+methodName.getName()+".xlsx");
 		ExcelLibrary excel =  new ExcelLibrary(file.getAbsolutePath());
 		 Object data[][] =	excel.readFromExcelDataForTestNGDataProvider(file.getAbsolutePath());
+		return data;
+	}
+	
+	@DataProvider(name="singleExcelMultipleSheets",parallel = true)
+	public static Object[][] singleExcelMultipleSheets(Method methodName) throws Exception{
+		
+		ApplicationSetUpPropertyFile setUp = new ApplicationSetUpPropertyFile();
+		File file = new File("resources/"+setUp.getProductName()+"TestData.xlsx");
+		ExcelLibrary excel =  new ExcelLibrary(file.getAbsolutePath(),methodName.getName());
+		 Object data[][] =	excel.readFromExcelDataForTestNGDataProvider(file.getAbsolutePath(),methodName.getName());
 		return data;
 	}
 }
--- src/test/java/org/google/modules/SearchModuleTest.java
+++ src/test/java/org/google/modules/SearchModuleTest.java
@@ -1,4 +1,8 @@
 package org.google.modules;
+import java.io.File;
+import java.io.IOException;
+
+import org.framework.utils.ExcelLibrary;
 import org.google.dataprovider.DataDrivenTestingFromExcel;
 import org.google.initializer.PageInitializer;
 import org.testng.Assert;
@@ -13,7 +17,7 @@ public class SearchModuleTest extends PageInitializer{
 	
 	@Features("Search Module")
 	@Stories(value = { "Search Text Scenarios" })
-	@Test(groups="regression",dataProvider="excelSheetDataRead",dataProviderClass=DataDrivenTestingFromExcel.class)
+	@Test(enabled=false,groups="regression",dataProvider="excelSheetDataRead",dataProviderClass=DataDrivenTestingFromExcel.class)
 	@TestCaseId("{0}")
     public void searchText_Scenarios(String testCaseId,String searchText)throws Exception {
 		 
@@ -22,9 +26,51 @@ public class SearchModuleTest extends PageInitializer{
 	
 	@Features("Search Module")
 	@Stories(value = { "Search Fail Scenarios" })
-	@Test(groups="regression")
+	@Test(enabled=false,groups="regression")
 	@TestCaseId("TC_002")
     public void failToAttachScreenshot()throws Exception {	 
 		Assert.assertTrue(false);
     }
+	
+	@Features("Search Module")
+	@Stories(value = { "Data Provider Testing Via Single excel but multiple sheets" })
+	@Test(enabled=false,groups="regression",dataProvider="singleExcelMultipleSheets",dataProviderClass=DataDrivenTestingFromExcel.class)
+	@TestCaseId("TC_003")
+    public void loginTest1(String testCaseId,String userName,String password,String expectedNameOfTheUser)throws Exception {	 
+		System.out.println(testCaseId);
+		System.out.println(userName);
+		System.out.println(password);
+		System.out.println(expectedNameOfTheUser);
+	}
+	
+	@Features("Search Module")
+	@Stories(value = { "Data Provider Testing Via Single excel but multiple sheets with multiple test data" })
+	@Test(enabled=false,groups="regression",dataProvider="singleExcelMultipleSheets",dataProviderClass=DataDrivenTestingFromExcel.class)
+	@TestCaseId("TC_004")
+    public void loginTest2(String testCaseId,String userName,String password,String expectedNameOfTheUser)throws Exception {
+	
+		
+		System.out.println(testCaseId);
+		System.out.println(userName);
+		System.out.println(password);
+		System.out.println(expectedNameOfTheUser);
+	}
+	
+	@Test
+	public void loginTest3() throws IOException{
+		File file = new File("resources/GoogleTestData.xlsx");
+		ExcelLibrary excel = new ExcelLibrary(file.getAbsolutePath(),"loginTest2");		
+		
+		System.out.println(excel.xlsxReadCell("Password", 2));
+		
+		//for row number and column number reading, make sure you provider 1 value less for the respective row number and the column number. 
+		System.out.println(excel.xlsxReadCell(0, 2));
+		
+		ExcelLibrary excel1 = new ExcelLibrary(file.getAbsolutePath(),"loginTest1");
+		
+		System.out.println(excel1.xlsxReadCell("Password", 1));
+		//for row number and column number reading, make sure you provider 1 value less for the respective row number and the column number. 
+		
+		System.out.println(excel1.xlsxReadCell(3, 1));
+	}
 }
